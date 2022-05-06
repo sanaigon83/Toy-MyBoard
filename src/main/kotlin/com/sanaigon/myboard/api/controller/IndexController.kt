aa9d9675b34b216ce6b7fccc2becc3166914147f
sanaigon83 <jk_sniper@naver.com>
2022-05-06 23:09:19 +0900

    add to update posts feature

--- src/main/kotlin/com/sanaigon/myboard/api/controller/IndexController.kt
+++ src/main/kotlin/com/sanaigon/myboard/api/controller/IndexController.kt
@@ -1,12 +1,14 @@
 package com.sanaigon.myboard.api.controller
 
+import com.sanaigon.myboard.api.dto.PostsResponseDto
 import com.sanaigon.myboard.service.PostsService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class IndexController (val postsService: PostsService) {
+class IndexController(val postsService: PostsService) {
 
     @GetMapping("/")
     fun index(model: Model): String {
@@ -16,4 +18,11 @@ class IndexController (val postsService: PostsService) {
 
     @GetMapping("/posts/save")
     fun postsSave() = "posts-save"
+
+    @GetMapping("/posts/update/{id}")
+    fun psotsUpdate(@PathVariable id: Long, model: Model): String {
+        val dto: PostsResponseDto = postsService.findById(id)
+        model.addAttribute("post", dto)
+        return "posts-update"
+    }
 }
